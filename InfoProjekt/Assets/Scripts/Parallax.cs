ede49713eb9b2812e421733cd30a1ae16d9d0d8a
dolqh1n <Mom>
2021-10-23 17:02:10 +0200

    Parallax Rework

--- InfoProjekt/Assets/Scripts/Parallax.cs
+++ InfoProjekt/Assets/Scripts/Parallax.cs
@@ -1,30 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform player;
-    private Vector2 startPosition;
-    private float startZ;
-
-    private Vector2 travel => (Vector2)cam.transform.position - startPosition; // die distanz die sich die cam von der startposition bewegt hat
-    private float distanceFromPlayer => transform.position.z - player.position.z; // die distanz in z richtung des objekts vom spieler
-
-    private float clippingPlane => (cam.transform.position.z + (distanceFromPlayer < 0 ? cam.farClipPlane : cam.nearClipPlane)); // wenn hinter dem spieler farClippingPlane(50) + camZ(-25) = 25 sonst nearClippingPlane(0) + CamZ(-25) = -25 also die koordinaten der clipping planes | clipping planes sind die grenzen was man noch in der cam sehen kann also alles von 0 (direkt an der cam) bis 50 weit weg von der cam
-    private float parallaxFactor => Mathf.Abs(distanceFromPlayer) / clippingPlane; // parallax factor des objekts von -1 bis 1 (wie schnell sich das objekt bewegt)
+    private Vector3 startPosition;
+    private float camStartX;
+    private float camTravel => cam.transform.position.x - camStartX;
+    private float parallaxFactor;
 
     public void Start()
     {
         startPosition = transform.position;
-        startZ = transform.position.z;
+        camStartX = cam.transform.position.x;
+        float clipFar = cam.farClipPlane;
+        parallaxFactor = transform.position.z / clipFar;
+        if (parallaxFactor > clipFar)
+            parallaxFactor = clipFar;
     }
 
     public void Update()
     {
-        Vector2 newPos = startPosition + travel * parallaxFactor; // neue position des objekts
-        transform.position = new Vector3(newPos.x, transform.position.y, startZ);
+        float newPosX = startPosition.x + camTravel * -parallaxFactor; 
+        transform.position = new Vector3(newPosX, transform.position.y, startPosition.z);
     }
     
 }
