62f6f47a44901953cf4630eebc7b0b3add3d753a
dolqh1nslayer69 <happydolphin42@gmail.com>
2021-11-15 18:12:28 +0100

    Main Menu v1
    Mit Scene laden und so

---
+++ InfoProjekt/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    private Button playButton;
+    private Button settingsButton;
+    private Button settingsBackButton;
+
+    private VisualElement settingsScreen;
+    private VisualElement mainMenuScreen;
+
+    void Start()
+    {
+        //visual element root also wo alles drinne is getten
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        //buttons vom root getten
+        playButton = root.Q<Button>("play_button");
+        settingsButton = root.Q<Button>("settings_button");
+        settingsBackButton = root.Q<Button>("back_button");
+
+        //settings und main menu screen getten
+        settingsScreen = root.Q<VisualElement>("settings");
+        mainMenuScreen = root.Q<VisualElement>("main_menu");
+
+        //die Funktionen den Buttons hinzufügen
+        playButton.clicked += playButtonPressed;
+        settingsButton.clicked += settingsButtonPressed;
+        settingsBackButton.clicked += settingsBackButtonPressed;
+    }
+
+    void playButtonPressed()
+    {
+        SceneManager.LoadScene("Spawn");
+    }
+
+    void settingsButtonPressed()
+    {
+        settingsScreen.style.display = DisplayStyle.Flex;
+        mainMenuScreen.style.display = DisplayStyle.None;
+    }
+
+    void settingsBackButtonPressed()
+    {
+        settingsScreen.style.display = DisplayStyle.None;
+        mainMenuScreen.style.display = DisplayStyle.Flex;
+    }
+}
