51ad9f29fe26506c97b62ad4f5ceea577df8acf8
dolqh1nslayer69 <happydolphin42@gmail.com>
2022-03-26 10:42:26 +0100

    Service Initialization

--- InfoProjekt/Assets/Scripts/Player/PlayerCombatController.cs
+++ InfoProjekt/Assets/Scripts/Player/PlayerCombatController.cs
@@ -36,8 +36,9 @@ namespace Player
         {
             Debug.Log("Attack");
             Vector2 mousePos = Input.mousePosition;
+            // wenn die maus rechts vom player is und der player nich nach rechts zeigt (x scale = negativ) || und anders rum       (240 sind die PPU) (4 sind die units vom rand links der cam bis zur mitte der cam)
             if((mousePos.x/240 - 4 > transform.position.x - camTransform.position.x && Math.Abs(Mathf.Sign(transform.localScale.x) - (-1)) > 0.01f) || (mousePos.x/240 - 4 < transform.position.x - camTransform.position.x && Math.Abs(Mathf.Sign(transform.localScale.x) - 1) > 0.01f))
-            {// wenn die maus rechts vom player is und der player nich nach rechts zeigt (x scale = negativ) || und anders rum       (240 sind die PPU) (4 sind die units vom rand links der cam bis zur mitte der cam)
+            {
                 var transformers = transform;
                 Vector3 scale = transformers.localScale;
                 scale.x *= -1;
