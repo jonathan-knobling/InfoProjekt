2e0ac1dafaa92ea065696b146516612e0dfef694
Carlo <bene.carloo@gmail.com>
2022-03-28 21:16:48 +0200

    Name Change

---
+++ InfoProjekt/Assets/Carlo/Scripts/Ore.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+
+namespace Inventory.Items
+{
+    [CreateAssetMenu(fileName = "New Ore", menuName = "Items/Ore")]
+    public class Ore : StackableItem
+    {
+        [SerializeField] private string type;
+    }
+}
