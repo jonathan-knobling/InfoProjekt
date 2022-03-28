15b7479a6b0164fb8d572f2b827adbccf3530309
Carlo <bene.carloo@gmail.com>
2022-03-28 21:16:48 +0200

    Name Change

--- InfoProjekt/Assets/Carlo/Scripts/Collectable.cs
+++ InfoProjekt/Assets/Carlo/Scripts/Collectable.cs
@@ -1,13 +1,52 @@
-
+using Gameplay.Inventory;
+using Gameplay.Inventory.Items;
+using Tech.IO;
+using UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
-namespace Inventory.Items
+public class Collectable : MonoBehaviour
 {
-    [CreateAssetMenu(fileName = "New Collectable", menuName = "Items/Collectables")]
-    public class Collectable : StackableItem
+    [SerializeField] private InputChannelSO inputChannel;
+    [SerializeField] private float interactionRadius;
+    [SerializeField] private LayerMask interactionLayer;
+    [SerializeField] private Item item;
+    [SerializeField] private UIChannelSO uiChannel;
+    private Label text;
+    void Start()
     {
-        [SerializeField] private string type;
-        [SerializeField] public Sprite image;
+        inputChannel.OnInteractButtonPressed += InteractButtonPressed;
+    }
+    
+    private void InteractButtonPressed()
+    {
+        if (Physics2D.OverlapCircle(transform.position, interactionRadius, interactionLayer))
+        {
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        Destroy(gameObject);
+        InventoryManager.Instance.AddItem(item);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, interactionRadius);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        text = new Label();
+        text.text = "Press F to pick up!";
+        text.transform.position = new Vector2(900, 900);
+        text.style.fontSize = 40;
+        uiChannel.RequestAddUIVisualElement(text);
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        uiChannel.RequestRemoveUIVisualElement(text);
     }
 }
