29a15d925fb9b6404b074c95929a96a4a9fe62e8
dolqh1nslayer69 <happydolphin42@gmail.com>
2022-03-29 16:59:32 +0200

    Stats Refactor

--- InfoProjekt/Assets/Scripts/Environment/Actors/Enemies/EnemyStats.cs
+++ InfoProjekt/Assets/Scripts/Environment/Actors/Enemies/EnemyStats.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 namespace Environment.Actors.Enemies
 {
-    public class EnemyStats : Stats
+    public class EnemyStats : MonoBehaviour, IDamagable
     {
         [Header("Animator")]
         private static readonly int AnimatorHit = Animator.StringToHash("hit");
         private static readonly int Death = Animator.StringToHash("death");
         [SerializeField] public Animator animator;
         
-        [Header("Stats")] 
+        [Header("Stats")]
         [SerializeField] public string enemyID;
         [SerializeField] public int level = 1;
         [SerializeField] private float maxHealth = 10;
@@ -20,34 +20,34 @@ namespace Environment.Actors.Enemies
         private const float LevelMultiplier = 2.5f;
         private float HiddenMaxHealth => (LevelMultiplier * level * maxHealth);
         private float HiddenAttackDamage => (int)(LevelMultiplier * level * attackDamage);
-        private new float Health;
+        private float health;
 
         //getters
-        public new int Level => level;
-        public bool IsDead => Health <= 0;
+        public int Level => level;
+        public bool IsDead => health <= 0;
         public int XPAmount => xpAmount;
-        public new float AttackDamage => HiddenAttackDamage;
-        public new float Speed => (float) ((1 + 0.25 * level) * 5);
+        public float AttackDamage => HiddenAttackDamage;
+        public float Speed => (float) ((1 + 0.25 * level) * 5);
         public float RoamingSpeed => (float) ((1 + 0.15 * level) * 5);
 
         private void Start()
         {
-            Health = HiddenMaxHealth;
+            health = HiddenMaxHealth;
         }
 
         [Description("Returns the actually dealt damage")]
-        public override float DealDamage(float damage)
+        public float DealDamage(float damage)
         {
             float actualDamage = damage * defenseMultiplier;
-            Health -= damage;
+            health -= damage;
             
             
-            if (Health <= 0)
+            if (health <= 0)
             {
                 Die();
                 
                 //relevant wenn health < 0
-                actualDamage -= Health;
+                actualDamage -= health;
                 return actualDamage;
             }
 
