8cda78d6a6d5c0d6d5c7641fd64c38e6228ccd76
Carlo <bene.carloo@gmail.com>
2022-04-09 22:35:42 +0200

    Hover movement script

--- InfoProjekt/Assets/Carlo/Scripts/TouchGrass.cs
+++ InfoProjekt/Assets/Carlo/Scripts/TouchGrass.cs
@@ -1,44 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Environment.Actors.Player;
-using Packages.Rider.Editor.UnitTesting;
-using UnityEditor;
 using UnityEngine;
 
-public class TouchGrass : MonoBehaviour
+namespace Assets.Carlo.Scripts
 {
-    [SerializeField] private PlayerMovementChannelSO movementChannel;
-    [SerializeField] private Animator grassAnimator;
-    private AudioSource grassSound;
-    private bool isTouching;
-    
-    private void Start()
+    public class TouchGrass : MonoBehaviour
     {
-        grassSound = GetComponent<AudioSource>();
-    }
+        [SerializeField] private PlayerMovementChannelSO movementChannel;
+        [SerializeField] private Animator grassAnimator;
+        private AudioSource grassSound;
+        private bool isTouching;
     
-    private void Update()
-    {
-        if (isTouching && movementChannel.Velocity > 0.0001f)
+        private void Start()
         {
-            grassAnimator.SetBool("GrassAnimation", true);
-            grassSound.Play();
+            grassSound = GetComponent<AudioSource>();
+        }
+    
+        private void Update()
+        {
+            if (isTouching && movementChannel.Velocity > 0.0001f)
+            {
+                grassAnimator.SetBool("GrassAnimation", true);
+                grassSound.Play();
+            }
+
+            if (!isTouching || movementChannel.Velocity < 0.0001f)
+            {
+                grassAnimator.SetBool("GrassAnimation" , false);
+            }
+        }
+    
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            isTouching = true;
         }
 
-        if (!isTouching || movementChannel.Velocity < 0.0001f)
+        private void OnTriggerExit2D(Collider2D col)
         {
-            grassAnimator.SetBool("GrassAnimation" , false);
+            isTouching = false;
         }
-    }
-    
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        isTouching = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        isTouching = false;
     }
 }
